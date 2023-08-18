fafea594df49b7c0cb4e58418f80925ca223ce30
= <androvidd380@gmail.com>
2023-08-17 20:11:27 -0600

    form react

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.target)
+
+    console.log("submit")
+
+    const url = "https://lovelatnet.com/api/login/login"
+
+    fetch(url, {
+      method: "POST",
+      body: formData
+    })
+    .then(response => response.json())
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name='mail' placeholder='Correo Electronico' />
+        <input type="password" name='password' placeholder='Contraseña' />
+        <input type="submit"/>
+      </form>
+    </>
+  )
+}
+
+export default App
